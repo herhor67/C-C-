e72444f561849dee3c9b0653e0a898e9df44df25
herhor67 <herhor67@interia.pl>
2020-01-24 16:09:33 +0100

    Delete str_vec_rem_empty.cpp

--- str_vec_rem_empty.cpp
+++
@@ -1,8 +0,0 @@
-
-void str_vec_rem_empty(vector<string>&);
-
-
-void str_vec_rem_empty(vector<string>& vect)
-{
-	vect.erase(partition(vect.begin(), vect.end(), [](const string& str) { return !str_empty(str); }), vect.end());
-}
