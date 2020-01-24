e1adc96d3fedd358d29c1ea9b535dac1510283d5
herhor67 <herhor67@interia.pl>
2020-01-24 11:31:04 +0100

    Create str_explode.cpp

---
+++ str_explode.cpp
@@ -0,0 +1,15 @@
+
+vector<string> explode(const string&, char);
+
+
+vector<string> explode(string const& s, char delim)
+{
+	vector<string> result;
+	istringstream iss(s);
+
+	for (string token; getline(iss, token, delim); )
+	{
+		result.push_back(move(token));
+	}
+	return result;
+}
