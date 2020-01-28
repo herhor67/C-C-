7a4a5811e841a2d21c8a36142e175c790907c68f
herhor67 <herhor67@interia.pl>
2020-01-28 12:22:53 +0100

    Create primes.cpp

---
+++ math/primes.cpp
@@ -0,0 +1,23 @@
+
+vector<int> primes(int limit)
+{
+	vector<int> primes = { 2 };
+	for (int number = 3; number <= limit; number += 2)
+	{
+		int maxDiv = integerSqrt(number) + 1;
+		bool isPrime = true;
+		for (const int& prime : primes)
+		{
+			if (prime > maxDiv)
+				break;
+			if (prime != 1 && number % prime == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+		if (isPrime)
+			primes.push_back(number);
+	}
+	return primes;
+}
