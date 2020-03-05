8d2436c77e2aa8cee8b5a7c68e6dfaf158a05258
herhor67 <herhor67@interia.pl>
2020-03-05 15:23:27 +0100

    Update varint.cpp

--- math/varint.cpp
+++ math/varint.cpp
@@ -9,8 +9,15 @@ using ull = unsigned long long;
 using unit = unsigned long long;
 constexpr size_t unitbyte = sizeof(unit);
 
+template <typename T>
+T gvoe(const vector<T> &vec, size_t pos)
+{
+	if (pos >= vec.size())
+		return T();
+	return vec[pos];
+}
 
-vector<string> str_split_backward(const string& str, size_t split)
+vector<string> str_split_backward(const string &str, size_t split)
 {
 	vector<string> out;
 	out.reserve(str.length() / split + (str.length() % split) ? 1 : 0);
@@ -22,7 +29,7 @@ vector<string> str_split_backward(const string& str, size_t split)
 		out.push_back(str.substr(i, split));
 	return out;
 }
-vector<string> str_split(const string& str, size_t split)
+vector<string> str_split(const string &str, size_t split)
 {
 	vector<string> out;
 	out.reserve(str.length() / split + (str.length() % split) ? 1 : 0);
@@ -30,7 +37,7 @@ vector<string> str_split(const string& str, size_t split)
 		out.push_back(str.substr(i, split));
 	return out;
 }
-vector<char> str_explode(const string& str)
+vector<char> str_explode(const string &str)
 {
 	vector<char> out;
 	out.reserve(str.length());
@@ -49,7 +56,7 @@ string num2bin(unit num)
 string num2hex(unit num)
 {
 	string temp;
-	for (unit i = (unit)1 << (unitbyte * 8 - 1); i != 0; i >>= 1)
+	for (unit i = unit(1) << (unitbyte * 8 - 1); i != 0; i >>= 1)
 	{
 		char dec = bool(num & i);
 		i >>= 1;
@@ -59,7 +66,7 @@ string num2hex(unit num)
 		i >>= 1;
 		dec = (dec << 1) | static_cast<char>(static_cast<bool>(num & i));
 		char hex = '0';
-		if      (dec <= 9)
+		if (dec <= 9)
 			hex = dec + 48;
 		else if (dec <= 15)
 			hex = dec + 65 - 10;
@@ -72,7 +79,7 @@ string oct2bin(string oct)
 {
 	vector<char> chars = str_explode(oct);
 	string bin;
-	bin.reserve(3*oct.length());
+	bin.reserve(3 * oct.length());
 	for (char chr : chars)
 	{
 		char dec = chr - 48;
@@ -107,7 +114,7 @@ vector<ull> primes(ull limit)
 	{
 		ull maxDiv = integerSqrt(number) + 1;
 		bool isPrime = true;
-		for (const ull& prime : primes)
+		for (const ull &prime : primes)
 		{
 			if (prime > maxDiv)
 				break;
@@ -123,26 +130,27 @@ vector<ull> primes(ull limit)
 	return primes;
 }
 
-
 class varint
 {
-public:
-	bool sign = 0;
+  public:
+	bool pstv = 1;
 	vector<unit> value;
-	varint(unit num)
+	varint(unit num, bool noneg = 1)
 	{
+		pstv = noneg;
 		value.push_back(num);
 	}
-	varint(string num, size_t base = 16)
+	varint(string num, size_t base = 16, bool noneg = 1)
 	{
-		if      (base == 16) // hex
+		pstv = noneg;
+		if (base == 16) // hex
 		{
 			vector<string> pieces = str_split_backward(num, unitbyte * 2);
 			value.reserve(pieces.size());
 			for (vector<string>::reverse_iterator i = pieces.rbegin(); i != pieces.rend(); ++i)
 				value.push_back(stoull(*i, 0, 16));
-//			for (string piece : pieces)
-//				value.push_back(stoull(piece, 0, 16));
+			//			for (string piece : pieces)
+			//				value.push_back(stoull(piece, 0, 16));
 		}
 		else if (base == 8) // oct
 		{
@@ -150,8 +158,8 @@ public:
 			value.reserve(pieces.size());
 			for (vector<string>::reverse_iterator i = pieces.rbegin(); i != pieces.rend(); ++i)
 				value.push_back(stoull(*i, 0, 2));
-//			for (string piece : pieces)
-//				value.push_back(stoull(piece, 0, 2));
+			//			for (string piece : pieces)
+			//				value.push_back(stoull(piece, 0, 2));
 		}
 		else if (base == 2) // bin
 		{
@@ -159,172 +167,172 @@ public:
 			value.reserve(pieces.size());
 			for (vector<string>::reverse_iterator i = pieces.rbegin(); i != pieces.rend(); ++i)
 				value.push_back(stoull(*i, 0, 2));
-//			for (string piece : pieces)
-//				value.push_back(stoull(piece, 0, 2));
+			//			for (string piece : pieces)
+			//				value.push_back(stoull(piece, 0, 2));
 		}
 		else // non pow2 systems
 		{
-
 		}
 		reduce();
 	}
-	
+
 	varint& increment(unit incr, size_t pos = 0)
 	{
-		if (pos >= value.size())
-		{
-			value.reserve(pos);
-			for (size_t i = value.size(); i < pos; ++i)
-				value.at(i) = 0;
-		}
 		do
 		{
 //			cout << "(" << pos << ") " << incr << endl;
+			value.resize(pos+1);
+			for (size_t i = value.size(); i <= pos; ++i)
+				value.at(i-1) = 0;
 			bool a = value.at(pos) >> (unitbyte * 8 - 1);
 			bool b = incr >> (unitbyte * 8 - 1);
 			value.at(pos) += incr;
 			bool s = value.at(pos) >> (unitbyte * 8 - 1);
 			incr = (!s && (a || b) || a && b);
 			++pos;
-		}
-		while (incr != 0);
+		} while (incr != 0);
 		reduce();
 		return *this;
 	}
 
-	bool operator >  (const unit& rhs)
+	bool operator> (const varint &rhs) const
 	{
-		if (value.size() > 1)
+		if (pstv > rhs.pstv)
 			return true;
-		if (value.size() == 1 && value.at(0) > rhs)
-			return true;
-		return false;
-	}
-	bool operator >= (const unit& rhs)
-	{
-		if (value.size() > 1)
-			return true;
-		if (value.size() == 1 && value.at(0) >= rhs)
-			return true;
-		return false;
-	}
-	bool operator <  (const unit& rhs)
-	{
-		if (value.size() == 1 && value.at(0) < rhs)
-			return true;
-		return false;
-	}
-	bool operator <= (const unit& rhs)
-	{
-		if (value.size() == 1 && value.at(0) <= rhs)
-			return true;
-		return false;
-	}
-	bool operator == (const unit& rhs)
-	{
-		if (value.size() == 1 && value.at(0) == rhs)
-			return true;
-		return false;
-	}
-	bool operator != (const unit& rhs)
-	{
-		if (value.size() != 1 || value.at(0) != rhs)
-			return true;
-		return false;
-	}
-
-	bool operator >  (const varint& rhs)
-	{
-		if (value.size() > rhs.value.size())
-			return true;
-		if (value.size() < rhs.value.size())
+		if (pstv < rhs.pstv)
 			return false;
-		for (int i = value.size() - 1; i >= 0; --i)
+		size_t maxize = value.size() >= rhs.value.size() ? value.size() : rhs.value.size();
+		for (size_t i = maxize; i > 0; --i)
 		{
-			if (value.at(i) > rhs.value.at(i))
-				return true;
-			if (value.at(i) < rhs.value.at(i))
-				return false;
+			if (gvoe(value, i - 1) > gvoe(rhs.value, i - 1))
+				return pstv;
+			if (gvoe(value, i - 1) < gvoe(rhs.value, i - 1))
+				return !pstv;
 		}
 		return false;
 	}
-	bool operator >= (const varint& rhs)
+	bool operator>=(const varint &rhs) const
 	{
-		if (value.size() > rhs.value.size())
+		if (pstv > rhs.pstv)
 			return true;
-		if (value.size() < rhs.value.size())
+		if (pstv < rhs.pstv)
 			return false;
-		for (int i = value.size() - 1; i >= 0; --i)
+		size_t maxize = value.size() >= rhs.value.size() ? value.size() : rhs.value.size();
+		for (size_t i = maxize; i > 0; --i)
 		{
-			if (value.at(i) > rhs.value.at(i))
+			if (gvoe(value, i - 1) > gvoe(rhs.value, i - 1))
+				return pstv;
+			if (gvoe(value, i - 1) < gvoe(rhs.value, i - 1))
+				return !pstv;
+		}
+		return true;
+	}
+	bool operator< (const varint &rhs) const
+	{
+		if (pstv < rhs.pstv)
+			return true;
+		if (pstv > rhs.pstv)
+			return false;
+		size_t maxize = value.size() >= rhs.value.size() ? value.size() : rhs.value.size();
+		for (size_t i = maxize; i > 0; --i)
+		{
+			if (gvoe(value, i - 1) < gvoe(rhs.value, i - 1))
+				return pstv;
+			if (gvoe(value, i - 1) > gvoe(rhs.value, i - 1))
+				return !pstv;
+		}
+		return false;
+	}
+	bool operator<=(const varint &rhs) const
+	{
+		if (pstv < rhs.pstv)
+			return true;
+		if (pstv > rhs.pstv)
+			return false;
+		size_t maxize = value.size() >= rhs.value.size() ? value.size() : rhs.value.size();
+		for (int i = maxize - 1; i >= 0; --i)
+		{
+			if (gvoe(value, i) < gvoe(rhs.value, i))
 				return true;
-			if (value.at(i) < rhs.value.at(i))
+			if (gvoe(value, i) > gvoe(rhs.value, i))
 				return false;
 		}
 		return true;
 	}
-	bool operator <  (const varint& rhs)
+	bool operator==(const varint &rhs) const
 	{
-		if (value.size() < rhs.value.size())
-			return true;
-		if (value.size() > rhs.value.size())
-			return false;
-		for (int i = value.size() - 1; i >= 0; --i)
+		size_t maxize = value.size() >= rhs.value.size() ? value.size() : rhs.value.size();
+		for (int i = maxize - 1; i >= 0; --i)
 		{
-			if (value.at(i) < rhs.value.at(i))
-				return true;
-			if (value.at(i) > rhs.value.at(i))
-				return false;
-		}
-		return false;
-	}
-	bool operator <= (const varint& rhs)
-	{
-		if (value.size() < rhs.value.size())
-			return true;
-		if (value.size() > rhs.value.size())
-			return false;
-		for (int i = value.size() - 1; i >= 0; --i)
-		{
-			if (value.at(i) < rhs.value.at(i))
-				return true;
-			if (value.at(i) > rhs.value.at(i))
+			if (gvoe(value, i) != gvoe(rhs.value, i))
 				return false;
 		}
 		return true;
 	}
-	bool operator == (const varint& rhs)
+	bool operator!=(const varint &rhs) const
 	{
-		if (value.size() != rhs.value.size())
-			return false;
-		for (int i = value.size() - 1; i >= 0; --i)
+		size_t maxize = value.size() >= rhs.value.size() ? value.size() : rhs.value.size();
+		for (int i = maxize - 1; i >= 0; --i)
 		{
-			if (value.at(i) != rhs.value.at(i))
-				return false;
-		}
-		return true;
-	}
-	bool operator != (const varint& rhs)
-	{
-		if (value.size() != rhs.value.size())
-			return true;
-		for (int i = value.size() - 1; i >= 0; --i)
-		{
-			if (value.at(i) != rhs.value.at(i))
+			if (gvoe(value, i) != gvoe(rhs.value, i))
 				return true;
 		}
 		return false;
 	}
 
+	bool operator> (const unit& rhs) const
+	{
+		return *this > varint(rhs);
+	}
+	bool operator>=(const unit& rhs) const
+	{
+		return *this >= varint(rhs);
+	}
+	bool operator< (const unit& rhs) const
+	{
+		return *this < varint(rhs);
+	}
+	bool operator<=(const unit& rhs) const
+	{
+		return *this <= varint(rhs);
+	}
+	bool operator==(const unit& rhs) const
+	{
+		return *this == varint(rhs);
+	}
+	bool operator!=(const unit& rhs) const
+	{
+		return *this != varint(rhs);
+	}
+
+	varint operator-() const
+	{
+		varint temp(*this);
+		temp.pstv = !pstv;
+		return temp;
+	}
+	varint abs() const
+	{
+		varint temp(*this);
+		temp.pstv = 1;
+		return temp;
+	}
 
 	varint& operator+=(const unit& rhs)
 	{
 		return increment(rhs, 0);
 	}
-	varint  operator+ (const varint& rhs)
+	varint& operator+=(const varint& rhs)
+	{
+		for (size_t i = 0; i < rhs.value.size(); ++i)
+			increment(gvoe(rhs.value, i), i);
+		return *this;
+	}
+
+	varint  operator+(const varint &rhs) const
 	{
 		varint temp(*this);
-
+		temp += rhs;
 		return temp;
 	}
 
@@ -341,21 +349,21 @@ public:
 	{
 		string result = "0x\n";
 		for (unit piece : value)
-			result.append(num2hex(piece)+'\n');
+			result.append(num2hex(piece) + '\n');
 		return result;
 	}
 	string dec() const
 	{
 		string result = "0d\n";
 		for (unit piece : value)
-			result.append(to_string(piece)+'\n');
+			result.append(to_string(piece) + '\n');
 		return result;
 	}
 	string bin() const
 	{
 		string result = "0b\n";
 		for (unit piece : value)
-			result.append(num2bin(piece)+'\n');
+			result.append(num2bin(piece) + '\n');
 		return result;
 	}
 };
@@ -376,11 +384,15 @@ int main()
 	string hex = "1234567890ABCDEFFEDCBA9876543210FFFFFFFFFFFFFFFF";
 	string oct = "22150531704653633677766713523035452062041777777777777777777777";
 	string bin = "0000000000000000000000000000000000000000000010010001101000101011001111000100110101011110011011110111111111110110111001011101010011000011101100101010000110010000100001111111111111111111111111111111111111111111111111111111111111111";
-	varint num(hex, 16);
-//	cout << num.dec() << endl << num.hex() << endl << num.bin() << endl;
-
 	varint snd(hex, 16);
-//	snd += 1;
+	varint num(1);
+	cout << num.dec() << endl
+		 << num.hex() << endl
+		 << num.bin() << endl;
 
-	cout << (num != snd);
+	num += snd;
+
+	cout << num.dec() << endl
+		 << num.hex() << endl
+		 << num.bin() << endl;
 }
